3c58685fe06f50b243d16654d5caedc346bed37e
Misbah <misbahnoor3252@gmail.com>
2025-05-02 14:21:29 +0500

    issue fixed

--- src/Component/RecipeCard.jsx
+++ src/Component/RecipeCard.jsx
@@ -6,9 +6,9 @@ return(
     <div className='bg-white rounded-xl shadow-2xl overflow-hidden'>
       <img src={recipes.image} alt={recipes.title} className='w-full h-75 object-cover hover:scale-105 transition-all ease-in-out duration-400' />
       <div className='p-6 flex flex-col justify-center items-center'>
-        <h2 className='text-xl font-bold mb-2'>{recipes.title}</h2>
+        <h2 className='text-2xl font-bold mb-2'>{recipes.title}</h2>
         <p className='text-gray-700'>{recipes.description}</p>
-        <button className='px-4 py-3 bg-red-500 rounded-lg w-full text-white my-5 hover:bg-red-600 cursor-pointer'><Link to={`/recipe/${recipes.id}`}>View Details</Link></button>   
+        <button className='px-4 py-3 bg-red-500 rounded-lg w-full text-white my-5 hover:bg-red-600 cursor-pointer font-semibold text-lg'><Link to={`/recipe/${recipes.id}`}>View Details</Link></button>   
        </div>   
     </div>
 
--- src/Pages/RecipeDetails.jsx
+++ src/Pages/RecipeDetails.jsx
@@ -1,5 +1,7 @@
 import { useParams } from "react-router-dom";
 import recipes from "../data/recipe";
+import { PiCookingPotDuotone } from "react-icons/pi";
+import { FaClipboardList } from "react-icons/fa";
 
 const RecipeDetails =() => {
    const {id} = useParams();
@@ -13,18 +15,18 @@ const RecipeDetails =() => {
 
    return(
     <div className="bg-[#fffaf0] min-h-screen">
-    <div className="mx-auto px-10 pt-20 sm:pt-25 flex sm:gap-25 gap-10 flex-wrap container flex-col sm:flex-row justify-center items-center pb-10">
+    <div className="mx-auto px-10 pt-8 sm:pt-25 flex sm:gap-25 gap-10 flex-wrap container flex-col sm:flex-row justify-center items-center pb-10">
         <div className="mt-18 flex flex-col justify-center items-center">
         <h1 className="mt-9 text-center text-5xl font-bold mb-8 text-[#e54d62]">{recipe.title}</h1> 
-      <img src={recipe.image} className="h-85 rounded-full px-3 shadow-lg hover:scale-95 transition-all ease-in-out duration-300" alt={recipe.title} />
+      <img src={recipe.image} className="h-85 object-cover rounded-lg shadow-lg hover:scale-95 transition-all ease-in-out duration-300" alt={recipe.title} />
       <p className="text-lg my-7 text-neutral-900">{recipe.description}</p>
         </div>
       <div className="mb-10 sm:mb-0 sm:mt-20 mt-0 text-lg">
-      <h3 className="text-2xl font-semibold mb-2 text-[#fe8b2e]">Ingredients:</h3>
+      <h3 className="flex gap-2 items-center text-2xl font-semibold mb-2 text-[#fe8b2e]"><FaClipboardList />Ingredients:</h3>
       <ul className="text-neutral-900 list-disc pl-5 mb-4">
         {recipe.ingredients.map((item, index) => <li key={index}>{item}</li>)}
      </ul>
-     <h3 className="text-2xl font-semibold mb-2 text-[#fe8b2e]">Instructions:</h3>
+     <h3 className="text-2xl font-semibold mb-2 text-[#fe8b2e] flex items-center gap-2"><PiCookingPotDuotone />Instructions:</h3>
      <ol className="list-decimal pl-5 text-neutral-900">
       {recipe.instructions.map((step, index) => <li key={index}>{step}</li>)}    
     </ol>
